8b5251b24504748b0907eea47b1413c21eedce42
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2023-07-28 21:08:20 +0300

    Add project files.

---
+++ BigDataDashboard/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using BigDataDashboard.DAL.Entites;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace BigDataDashboard.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly string _connectionString = "Server =DESKTOP-D19MH5E\\SQLEXPRESS; initial catalog = CARPLATES; integrated security = true";
+        public async Task<IActionResult> Index()
+        {
+            await using var connection = new SqlConnection(_connectionString);
+            var values = (await connection.QueryAsync<PLATES>("Select * from PLATES")).AsList();
+            return View(values);
+        }
+    }
+}
